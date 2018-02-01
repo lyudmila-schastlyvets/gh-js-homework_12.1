179086eef21cbcf2da7f14d787e4f10004a8f7f4
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2018-02-01 14:13:58 +0200

    Improvements in code style

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,67 +73,64 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sass_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__sass_style_scss__);
 
 
-var currTransl = [];
+var translatesArray = [];
 var indexes = [];
 var translationComplete = true;
-var transitionCompleted = function() {
+var transitionCompleted = function () {
     translationComplete = true;
 };
 var startPoint  = 0;
 var direction;
 var move = false;
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var sliders = document.getElementsByClassName('single-slider-container');
     for (var i=0; i < sliders.length; i++) {
         indexes.push(0);
-        currTransl.push([]);
+        translatesArray.push([]);
     }
-    Array.prototype.forEach.call(sliders, function (el, ind) {
-        var container = el;
-        var containerWidth = container.clientWidth;
-        var amount = container.querySelectorAll('li').length;
+    Array.prototype.forEach.call(sliders, function (slider, ind) {
+        var containerWidth = slider.clientWidth;
+        var amount = slider.querySelectorAll('li').length;
         for (var i = 0; i < amount; i++) {
-            currTransl[ind][i] = -containerWidth;
-            container.querySelectorAll('li')[i].addEventListener('transitionend', transitionCompleted, true);
-            container.querySelectorAll('li')[i].addEventListener('webkitTransitionEnd', transitionCompleted, true);
-            container.querySelectorAll('li')[i].addEventListener('oTransitionEnd', transitionCompleted, true);
-            container.querySelectorAll('li')[i].addEventListener('MSTransitionEnd', transitionCompleted, true);
+            translatesArray[ind][i] = -containerWidth;
+            slider.querySelectorAll('li')[i].addEventListener('transitionend', transitionCompleted, true);
+            slider.querySelectorAll('li')[i].addEventListener('webkitTransitionEnd', transitionCompleted, true);
+            slider.querySelectorAll('li')[i].addEventListener('oTransitionEnd', transitionCompleted, true);
+            slider.querySelectorAll('li')[i].addEventListener('MSTransitionEnd', transitionCompleted, true);
 
-            container.querySelectorAll('li')[i].addEventListener('mousedown', function (event) {
-                var field = this.getBoundingClientRect();
-                startPoint = event.offsetX + field.x;
+            slider.querySelectorAll('li')[i].addEventListener('mousedown', function (event) {
+                startPoint = event.offsetX + this.getBoundingClientRect().x;
             }, true);
 
-            container.querySelectorAll('li')[i].addEventListener('mousemove', function (event) {
-                var movePoint = event.clientX;
-                direction = startPoint - movePoint;
+            slider.querySelectorAll('li')[i].addEventListener('mousemove', function (event) {
+                direction = startPoint - event.clientX;
                 if (startPoint && translationComplete) {
                     for (var i = 0; i < amount; i++) {
-                        container.querySelectorAll('li')[i]
-                            .style.transform = 'translate(' + (currTransl[ind][i] - direction ) + 'px)';
+                        slider.querySelectorAll('li')[i]
+                            .style.transform = 'translate(' + (translatesArray[ind][i] - direction) + 'px)';
                     }
                 }
                 move = true;
             }, true);
 
-            container.querySelectorAll('li')[i].addEventListener('mouseout', function () {
+            slider.querySelectorAll('li')[i].addEventListener('mouseout', function () {
                 if (startPoint) {
                     startPoint = 0;
                     if (direction > 0) {
-                        left(el, ind, amount);
+                        left(slider, ind, amount);
                     } else {
-                        right(el, ind, amount);
+                        right(slider, ind, amount);
                     }
                 }
             });
 
-            container.querySelectorAll('li')[i].addEventListener('mouseup', function () {
+            slider.querySelectorAll('li')[i].addEventListener('mouseup', function () {
                 if (move) {
                     if (direction > 0) {
-                        left(el, ind, amount);
+                        left(slider, ind, amount);
                     } else {
-                        right(el, ind, amount);
+                        right(slider, ind, amount);
                     }
                     direction = 0;
                     startPoint = 0;
@@ -141,25 +138,24 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }, true);
 
-            container.querySelectorAll('li')[i].addEventListener('touchstart', function (event) {
-                var movePoint = event.clientX;
-                direction = startPoint - movePoint;
+            slider.querySelectorAll('li')[i].addEventListener('touchstart', function (event) {
+                direction = startPoint - event.clientX;
                 if (startPoint) {
                     for (var i = 0; i < amount; i++) {
-                        container.querySelectorAll('li')[i]
-                            .style.transform = 'translate(' + (currTransl[ind][i] - direction) + 'px)';
+                        slider.querySelectorAll('li')[i]
+                            .style.transform = 'translate(' + (translatesArray[ind][i] - direction) + 'px)';
                     }
                 }
                 move = true;
             }, true);
 
-            container.querySelectorAll('li')[i].addEventListener('touchend', function () {
+            slider.querySelectorAll('li')[i].addEventListener('touchend', function () {
                 if (move) {
                     translationComplete = true;
                     if (direction > 0) {
-                        left(el, ind, amount);
+                        left(slider, ind, amount);
                     } else {
-                        right(el, ind, amount);
+                        right(slider, ind, amount);
                     }
                     direction = 0;
                     startPoint = 0;
@@ -168,11 +164,11 @@ document.addEventListener('DOMContentLoaded', function() {
             }, true);
         }
 
-        el.addEventListener('click', function (event) {
+        slider.addEventListener('click', function (event) {
             if (event.offsetX > 200) {
-                right(el, ind, amount);
+                right(slider, ind, amount);
             } else {
-                left(el, ind, amount);
+                left(slider, ind, amount);
             }
         });
     });
@@ -180,48 +176,48 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Right moving items
-function right (box, ind_param, amount) {
-    var container = box;
+function right (container, position, amount) {
     var containerWidth = container.clientWidth;
-    if (translationComplete === true) {
+    if (translationComplete) {
         translationComplete = false;
-        indexes[ind_param]=indexes[ind_param]-1;
-        if (indexes[ind_param] === -1) {
-            indexes[ind_param] = amount - 1;
+        indexes[position] = indexes[position] - 1;
+        if (indexes[position] === -1) {
+            indexes[position] = amount - 1;
         }
-        var outerIndex = (indexes[ind_param]) % amount;
+        var outerIndex = (indexes[position]) % amount;
         for (var i = 0; i < amount; i++) {
             var li = container.querySelectorAll('li')[i];
             li.style.opacity = '1';
-            li.style.transform = 'translate('+(currTransl[ind_param][i]+containerWidth)+'px)';
-            currTransl[ind_param][i] = currTransl[ind_param][i] + containerWidth;
+            li.style.transform = 'translate(' + (translatesArray[position][i] + containerWidth) + 'px)';
+            translatesArray[position][i] = translatesArray[position][i] + containerWidth;
         }
 
         var outerLi = container.querySelectorAll('li')[outerIndex];
-        outerLi.style.transform = 'translate('+(currTransl[ind_param][outerIndex]-containerWidth*(amount))+'px)';
+        outerLi.style.transform = 'translate(' + (translatesArray[position][outerIndex] - containerWidth * amount) + 'px)';
         outerLi.style.opacity = '0';
-        currTransl[ind_param][outerIndex] = currTransl[ind_param][outerIndex]-containerWidth*(amount);
+        translatesArray[position][outerIndex] = translatesArray[position][outerIndex] - containerWidth * amount;
     }
 }
 
 // Left moving items
-function left (box, ind_param, amount) {
-    var container = box;
+function left (container, position, amount) {
     var containerWidth = container.clientWidth;
-    if (translationComplete === true) {
+    if (translationComplete) {
         translationComplete = false;
-        indexes[ind_param]=indexes[ind_param]+1;
-        var outerIndex = (indexes[ind_param]-1) % amount;
+        indexes[position] = indexes[position] + 1;
+        var outerIndex = (indexes[position] - 1) % amount;
         for (var i = 0; i < amount; i++) {
             var li = container.querySelectorAll('li')[i];
             li.style.opacity = '1';
-            li.style.transform = 'translate('+(currTransl[ind_param][i]-containerWidth)+'px)';
-            currTransl[ind_param][i] = currTransl[ind_param][i]-containerWidth;
+            li.style.transform = 'translate(' + (translatesArray[position][i] - containerWidth) + 'px)';
+            translatesArray[position][i] = translatesArray[position][i] - containerWidth;
         }
         var outerLi = container.querySelectorAll('li')[outerIndex];
-        outerLi.style.transform = 'translate('+(currTransl[ind_param][outerIndex]+containerWidth*(amount))+'px)';
+        outerLi.style.transform =
+            'translate(' + (translatesArray[position][outerIndex] + containerWidth * amount) + 'px)';
         outerLi.style.opacity = '0';
-        currTransl[ind_param][outerIndex] = currTransl[ind_param][outerIndex]+containerWidth*(amount);
+        translatesArray[position][outerIndex] =
+            translatesArray[position][outerIndex] + containerWidth * amount;
     }
 }
 
